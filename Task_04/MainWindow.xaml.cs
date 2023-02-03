9b423609f1a523bdaa0968af82e9d0dcdbaef816
ArtemBekchurov <119742768+ArtemBekchurov@users.noreply.github.com>
2023-02-03 19:36:14 +0300

    Initial commit

---
+++ Task_04/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Task_04
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(car_1, Canvas.GetLeft(car_1) - 13);
+            Canvas.SetLeft(car_2, Canvas.GetLeft(car_2) - 17);
+            Canvas.SetLeft(car_3, Canvas.GetLeft(car_3) - 34);
+
+            Rect rect_car2 = new Rect(Canvas.GetLeft(car_2), Canvas.GetTop(car_2), car_2.Height, car_2.Width);
+            Rect rect_car3 = new Rect(Canvas.GetLeft(car_3), Canvas.GetTop(car_3), car_3.Height, car_3.Width);
+
+            if (rect_car2.IntersectsWith(rect_car3))
+            {
+                MessageBox.Show("СТОЛКНОВЕНИЕ!!!");
+            }
+        }
+    }
+
+}
